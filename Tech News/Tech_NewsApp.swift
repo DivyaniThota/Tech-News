8506a5a33925e308134f6095be1730d99f945856
DivyaniThota <divyaniprasad99@gmail.com>
2024-02-06 13:49:56 -0700

    Initial Commit

---
+++ Tech News/Tech_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tech_NewsApp.swift
+//  Tech News
+//
+//  Created by DIVYANI PRASAD THOTA on 06/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Tech_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
